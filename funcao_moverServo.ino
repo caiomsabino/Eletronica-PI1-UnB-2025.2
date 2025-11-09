12b7a6f0b94310d3da902bc5beaedaaa21290a83
juliajffernandes <98669918+juliajffernandes@users.noreply.github.com>
2025-11-09 01:28:01 -0300

    Add abrir e fechar a porta

--- funcao_moverServo.ino
+++ funcao_moverServo.ino
@@ -1,23 +1,25 @@
 #include <ESP32Servo.h>
 
-// ==== Definições ====
-Servo servoMotor;        // Cria o objeto servo
-const int servoPin = 25;  // Pino de controle do servo
+Servo servoMotor;
+const int servoPin = 25;
 
-// ==== Configuração inicial ====
-void setup() {
-  Serial.begin(115200);
-  servoMotor.attach(servoPin);  // Liga o servo ao pino definido
-}
-
-// ==== Função para mover o servo ====
 void moverServo(int angulo) {
-  // Limita o ângulo entre 0 e 180 graus
   angulo = constrain(angulo, 0, 180);
-
-  servoMotor.write(angulo);  // Envia o sinal para o servo
-  delay(500);  // Espera o servo chegar na posição
+  servoMotor.write(angulo);
+  delay(500);
 }
 
+void setup() {
+  servoMotor.attach(servoPin);  // liga o servo ao pino
+}
 
+//Função para abrir a porta
+void abrirPorta() {
+  moverServo(50);   // posição aberta
+}
+
+//Função para fechar a porta
+void fecharPorta() {
+  moverServo(160);  // posição fechada
+}
 
